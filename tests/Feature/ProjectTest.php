c9910e702aa2c3e096f6f84f6f5335b7272c3844
Tonio <antoine.doussan@campus-numerique-in-the-alps.com>
2018-05-16 13:33:38 +0200

    Class ProjectTest créée et test GET 200 ok

---
+++ tests/Feature/ProjectTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antoine.doussan
+ * Date: 16/05/2018
+ * Time: 13:16
+ */
+
+namespace Tests\Feature;
+
+
+use Tests\TestCase;
+
+class ProjectTest extends TestCase
+
+{
+    public function testBasicTest()
+    {
+        $response = $this->get('/http://127.0.0.1:8000/');
+
+        $response->assertStatus(200);
+    }
+}
